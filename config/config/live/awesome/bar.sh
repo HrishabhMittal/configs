636d84110e2772da9b1f95fc1dbe04a2e3be4129
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2025-07-24 17:16:25 +0530

    Revert "ad live wallpaper theme"
    This reverts commit 17bf8654fea8e56ed286bc5980ac5dd41c6fa17d.

--- config/config/live/awesome/bar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source $HOME/pyenv/general/bin/activate
-python $HOME/.config/awesome/bar/config.py
--- config/config/live/hypr/bar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source $HOME/pyenv/general/bin/activate
-python $HOME/.config/hypr/bar/config.py
